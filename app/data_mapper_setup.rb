1069c48a56566d262e23e9626813e0c4f9c52225
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-08-11 16:15:11 +0100

    heroku

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,7 +2,7 @@ require 'data_mapper'
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
 
 require './app/models/link'
 
