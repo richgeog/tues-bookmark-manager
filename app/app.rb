cc04924da10f5ee0960faae81e74ca5f0af358cf
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-08-12 15:47:57 +0100

    password confirmation added

--- app/app.rb
+++ app/app.rb
@@ -51,9 +51,10 @@ class BookmarkManager < Sinatra::Base
 
   post '/users' do
     user = User.create(email: params[:email],
-                password: params[:password])
+                password: params[:password],
+                password_confirmation: params[:password_confirmation])
     session[:user_id] = user.id
-    redirect to('/')
+    redirect to('/links')
   end
 
   # start the server if ruby file executed directly
