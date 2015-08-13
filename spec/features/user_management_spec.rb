4a854bb18e50ccc035dbc7f06ca0b12329da484d
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-08-13 10:27:39 +0100

    refactored user managemnet without factory girl

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,36 +1,32 @@
 feature 'User sign up' do
 
   scenario 'I can sign up as a new user' do
-    expect { sign_up }.to change(User, :count).by(1)
+    user = User.new(email: 'alice@example.com',
+                    password: '12345678',
+                    password_confirmation: '12345678')
+    expect { sign_up(user) }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq('alice@example.com')
   end
 
-  # def sign_up(email: 'alice@example.com',
-  #             password: 'oranges!')
-  #     visit 'users/new'
-  #     expect(page.status_code).to eq(200)
-  #     fill_in :email, with: email
-  #     fill_in :password, with: password
-  #     click_button 'Sign up'
-  #   end
-
   scenario 'requires a matching confirmation password' do
-    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    user = User.new(password_confirmation: 'wrong',
+                    email: 'alice@example.com',
+                    password: '12345678')
+    expect { sign_up(user) }.not_to change(User, :count)
   end
 
-  def sign_up(email: 'alice@example.com',
-            password: '12345678',
-            password_confirmation: '12345678')
+  def sign_up(user)
     visit '/users/new'
-    fill_in :email, with: email
-    fill_in :password, with: password
-    fill_in :password_confirmation, with: password_confirmation
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+    fill_in :password_confirmation, with: user.password_confirmation
     click_button 'Sign up'
   end
 
   scenario 'with a password that does not match' do
-    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    user = User.new(password_confirmation: 'wrong')
+    expect { sign_up(user) }.not_to change(User, :count)
     expect(current_path).to eq('/users')
     expect(page).to have_content 'Password and confirmation password do not match'
   end
