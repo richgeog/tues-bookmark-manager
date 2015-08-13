a2e9317868a3318385d90e2424f94208833a2073
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-08-13 16:12:53 +0100

    sign in process completed

--- app/app.rb
+++ app/app.rb
@@ -53,20 +53,33 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/users' do
+    # byebug
     @user = User.new(email: params[:email],
-                password: params[:password],
-                password_confirmation: params[:password_confirmation])
+                    password: params[:password],
+                    password_confirmation: params[:password_confirmation])
     if @user.save
       session[:user_id] = @user.id
       redirect to('/links')
-    elsif @user.email == ''
-      flash.now[:errors] = @user.errors.full_messages
-      erb :'users/new'
-    elsif @user.password_confirmation != @user.password
+    else
       flash.now[:errors] = @user.errors.full_messages
       erb :'users/new'
     end
   end
+
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions' do
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect to('/links')
+    else
+      flash.now[:errors] = ['The email or password is incorrect']
+      erb :'sessions/new'
+  end
+end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -51,4 +51,24 @@ feature 'User sign up' do
     click_button 'Sign up'
   end
 
+feature 'User sign in' do
+
+ let(:user) do
+   User.create(email: 'user@example.com',
+               password: 'secret1234',
+               password_confirmation: 'secret1234')
+ end
+
+   scenario 'with correct credentials' do
+     sign_in(user)
+     expect(page).to have_content "Welcome, #{user.email}"
+   end
+
+   def sign_in(user)
+     visit '/sessions/new'
+     fill_in 'email', with: user.email
+     fill_in 'password', with: user.password
+     click_button 'Sign in'
+   end
+  end
 end
